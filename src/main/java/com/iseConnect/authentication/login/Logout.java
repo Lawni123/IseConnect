aca965d1eb8a1f6274ef4b155411ea9778da5566
Lawni123 <lawnilaxmikant07@gmail.com>
2025-05-15 23:46:54 +0530

    Initial commit for Render deployment

---
+++ src/main/java/com/iseConnect/authentication/login/Logout.java
@@ -0,0 +1,23 @@
+package com.iseConnect.authentication.login;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("uname");
+		session.invalidate();
+		response.sendRedirect("index.html");
+	}
+
+}
